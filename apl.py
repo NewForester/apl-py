70d47cd13d84e56405d870b1c3ff3425a43cb949
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-03-29 17:00:00 +0100

    Add (dummy) calculator mode

--- apl.py
+++ apl.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 """
     A simple wrapper script for the APL Read-Evaluate-Print loop
+
+    Supports 'shell' and 'calculator' modes
 """
 
-from rep import read_evaluate_print, apl_quit
+import sys
+
+from functools import reduce
+
+from rep import read_evaluate_print, apl_quit, evaluate
+
+# ------------------------------
 
 if __name__ == '__main__':
-    print("APL Read-Evaluate-Print Loop.")
-    try:
-        read_evaluate_print ('       ')
-    except KeyboardInterrupt:
-        apl_quit()
+    if len(sys.argv) == 1:
+        print("APL Shell")
+
+        try:
+            read_evaluate_print('       ')
+        except KeyboardInterrupt:
+            apl_quit()
+    else:
+        # evaluate parameters as an APL expresson
+
+        print(evaluate(reduce(lambda x, y: x + ' ' + y, sys.argv[1:])))
 
 # EOF
