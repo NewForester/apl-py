bf74dbb7c17187670cf17939c5d7493d7e2aa762
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-05-27 11:57:49 +0000

    Add support for scripting - closes #32

--- apl.py
+++ apl.py
@@ -32,12 +32,12 @@ def     print_result (result,prefix=""):
 
 # ------------------------------
 
-def     print_error (error,expr,prompt=""):
+def     print_error (error,expr,prompt="",where=""):
     """
     print the error response when APL expression evaluation fails
     """
     print(' '*(len(prompt)+len(expr)-len(error.expr.lstrip())),end="^\n")
-    print(error.message)
+    print(error.message,where)
 
 # ------------------------------
 
@@ -54,7 +54,39 @@ def     _strip_comment (line):
 
 # ------------------------------
 
-def     read_evaluate_print (prompt):
+def     rep_from_file (prompt,path,inputFile,silent):
+    """
+    read input, evaluate it and print the result
+    """
+    if silent:
+        prefix = ""
+    else:
+        prefix = "⎕"
+
+    lineCount = 0
+    for line in inputFile:
+        lineCount += 1
+
+        if lineCount == 1:
+            if line.startswith("#!"):
+                continue
+
+        if not silent:
+            print("{0}{1}".format(prompt,line),end="")
+
+        expr = _strip_comment(line)
+        if expr:
+            try:
+                print_result(evaluate(expr),prefix)
+            except apl_exception as error:
+                if silent:
+                    print("{0}{1}".format(prompt,line),end="")
+                print_error(error,expr,prompt,"in {0} on line {1}".format(path,lineCount))
+                apl_exit(1)
+
+# ------------------------------
+
+def     rep_from_tty (prompt):
     """
     read input, evaluate it and print the result
     """
@@ -71,28 +103,72 @@ def     read_evaluate_print (prompt):
 # ------------------------------
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print("APL Shell implemented in Python 3, Copyright 2017 NewForester")
+    try:
+        if len(sys.argv) == 1:
+            flags, args = [], []
+        elif '--' in sys.argv:
+            ii = sys.argv.index("--")
+            flags, args = sys.argv[1:ii], sys.argv[ii + 1:]
+        elif sys.argv[1][0] == '-':
+            flags, args = sys.argv[1:], []
+        else:
+            flags, args = [], sys.argv[1:]
 
-        try:
-            read_evaluate_print('       ')
-        except EOFError:
-            apl_exit(0,"")
-        except KeyboardInterrupt:
-            apl_quit(2,"^C")
-    else:
-        # evaluate parameters as an APL expresson
+        if len(flags):
+            flags = ' '.join(flags).replace(' -',':-').split(':')
 
-        line = ' '.join(sys.argv[1:])
+        silent = False
 
-        expr = _strip_comment(line)
-        if expr:
-            try:
-                print_result(evaluate(expr))
-                apl_exit(0)
-            except apl_exception as error:
-                print(line)
-                print_error(error,expr)
-                apl_exit(1)
+        for flag in flags:
+            if flag in ("-h", "--help"):
+                print("{0} coming soon".format(flag))
+
+            elif flag in ("-s", "--silent", "--script"):
+                silent = True
+
+            elif flag in ("-v", "--verbose"):
+                silent = False
+
+            elif flag.startswith(("-f ", "--file ")):
+                path = flag.split()[1];
+                inputFile = open(path, "r")
+                rep_from_file('       ',path,inputFile,silent)
+                inputFile.close()
+
+            else:
+                print("{0} not recognised: perhaps try --help".format(flag))
+
+        if sys.stdin.isatty():
+            # enter interactive shell iff there are is no command line expression
+
+            if not len(args):
+                print("APL Shell implemented in Python 3, Copyright 2017 NewForester")
+
+                try:
+                    rep_from_tty('       ')
+                except EOFError:
+                    apl_exit(0,"")
+        else:
+            # stdin redirected
+
+            rep_from_file('       ',"<stdin>",sys.stdin,silent)
+
+        if len(args):
+            # evaluate command line expression
+
+            line = ' '.join(args)
+
+            expr = _strip_comment(line)
+            if expr:
+                try:
+                    print_result(evaluate(expr))
+                    apl_exit(0)
+                except apl_exception as error:
+                    print(line)
+                    print_error(error,expr)
+                    apl_exit(1)
+
+    except KeyboardInterrupt:
+        apl_quit(2,"^C")
 
 # EOF
