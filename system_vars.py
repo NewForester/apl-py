4047eaa03d829321b17c48c0c92dd6e4e6fd459b
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-06-10 10:24:24 +0100

    Refactor system_vars.py

--- system_vars.py
+++ system_vars.py
@@ -16,9 +16,11 @@ from apl_error import apl_error
 
 # ------------------------------
 
-class   apl_system_variable(object):
+class   APL_system_variable(object):
     """
-    a simple pair: (value, confirm function)
+    an APL system variable is represented by a pair; the is no behaviour
+        - a value (represented by an instance of APL_quantity)
+        - a confirm function (used during assignment to confirm the quantity falls in the approrpriate domain)
     """
     def __init__(self,value,confirm):
         self.value = value
@@ -26,12 +28,54 @@ class   apl_system_variable(object):
 
 # ------------------------------
 
-def     equalCT (A,B):
-    return operator.le(math.fabs(A-B),comparisonTolerance.value.python() * max(math.fabs(A), math.fabs(B)))
+_indexOrigin =           APL_system_variable(apl_scalar(1),      lambda B: eval_monadic(confirm_bool,B))
+_comparisonTolerance =   APL_system_variable(apl_scalar(1e-13),  lambda B: eval_monadic(confirm_real,B))
+
+_system_variables = {
+    "IO":       _indexOrigin,
+    "CT":       _comparisonTolerance,
+}
 
 # ------------------------------
 
+def     system_variable (name,value=None):
+    """
+    set or get the value of a system variable (from the shell)
+
+    throws UNKNOWN SYSTEM VARIABLE if the name is not recognised
+    """
+    try:
+        sys_var = _system_variables[name.upper()]
+    except KeyError:
+        apl_error("UNKNOWN SYSTEM VARIABLE", name)
+
+    if not value is None:
+        sys_var.value = sys_var.confirm(value)
+
+    return sys_var.value
+
+# ------------------------------
+
+def     indexOrigin ():
+    """
+    the current index origin (0 or 1)
+    """
+    return _indexOrigin.value.python()
+
+# --------------
+
+def     equalCT (A,B):
+    """
+    true if A = B within the current comparison tolerance
+    """
+    return operator.le(math.fabs(A-B),_comparisonTolerance.value.python() * max(math.fabs(A), math.fabs(B)))
+
+# --------------
+
 def     integerCT (B):
+    """
+    returns B but if B is withhin comparison toleranace of an integer, returns the integer
+    """
     if not type(B) is int:
         if B > 0:
             integer = int(B+0.5)
@@ -45,9 +89,12 @@ def     integerCT (B):
 
     return B
 
-# ------------------------------
+# --------------
 
 def     confirm_bool (B):
+    """
+    return the Boolean-domain value of B (within comparison tolerance) - else throw DOMAIN ERROR
+    """
     B = integerCT(B)
 
     if B == 0:  return 0
@@ -55,9 +102,12 @@ def     confirm_bool (B):
 
     apl_error("DOMAIN ERROR")
 
-# ------------------------------
+# --------------
 
 def     confirm_int (B):
+    """
+    return the integer-domain value of B (within comparison tolerance) - else throw DOMAIN ERROR
+    """
     B = integerCT(B)
 
     if not type(B) is int:
@@ -65,39 +115,12 @@ def     confirm_int (B):
 
     return B
 
-# ------------------------------
+# --------------
 
 def     confirm_real (B):
+    """
+    return the real-domain value of B
+    """
     return float(B)
-
-# ------------------------------
-
-indexOrigin =           apl_system_variable(apl_scalar(1),      lambda B: eval_monadic(confirm_bool,B))
-comparisonTolerance =   apl_system_variable(apl_scalar(1e-13),  lambda B: eval_monadic(confirm_real,B))
-
-system_variables = {
-    "IO":       indexOrigin,
-    "CT":       comparisonTolerance,
-}
-
-# ------------------------------
-
-def     system_variable (name,value=None):
-    """
-    return the value of a system variable
-
-    if a value is passed in, it is assigned to the system variable and the value is returned
-
-    throws UNKNOWN SYSTEM VARIABLE if the name is not recognised
-    """
-    try:
-        sys_var = system_variables[name.upper()]
-    except KeyError:
-        apl_error("UNKNOWN SYSTEM VARIABLE", name)
-
-    if not value is None:
-        sys_var.value = sys_var.confirm(value)
-
-    return sys_var.value
 
 # EOF
