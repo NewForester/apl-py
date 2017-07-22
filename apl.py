21228c3dfa88f7a63c5912be15c4983eadb8298c
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-07-22 14:07:01 +0000

    Update the help text - closes #55, #57

--- apl.py
+++ apl.py
@@ -30,65 +30,17 @@ _helpText = """
 Invocation without any flags or arguments will enter an interactive APL shell.
 Use an APL command such as )OFF to exit.
 
-Invocation without flags but with arguments will intepret the arguments as
+Invocation without flags but with arguments will interpret the arguments as
 an APL expression, print the result and exit.  Eg:
 
     $ apl.py 4 8 16 ÷ 2
     2 4 8
 
-It is possible to log the entire interactive session to file:
-
-    $ apl.py -l log_file
-
-has the same effect as:
-
-    $ apl.py |& tee -i log_file
-
-except the second disables readline editing.
-
-Non-interactive (or scripted) use is supported:
-
-    $ apl.py < script_file
-    $ cat script_file | apl-py
-    $ apl.py -f script_file
-
-will all interpret script_file as a sequence of APL expressions.  Interactive
-output may be suppressed using the --silent flag.  In response to an error,
-the interpreter will print the offending line (and line number) and then exit.
-
-To be able to run a script directly from a Linux command line, the file must
-be executable and its first line should read something like:
-
-    # !/usr/bin/apl.py --silent -f
-
-Use the path appropriate for your installation.  The -f flag must appear to the
-right of any other flags.
-
-There is no mechanism for script files to include other script files but they
-may be chained together:
-
-   $ apl.py -f file_1 -f file_2 ...
-
-A standalone script file is expected to use )OFF or equivalent to terminate.
-Otherwise script files are assumed to be rc files that set up a particular
-environment (such as loading a workspace).
-
-If the interpreter is still running after executing the last line of the final
-script file, then it will consider entering the interactive shell or
-interpreting command line arguments.
-
-When working with scripts (and flags in general), -- indicates 'end of flags':
-any command line arguments further to the right may be interpreted as an APL
-expression:
-
-    $ apl.py -f load_application_workspace -- invoke_application
-
-If there are no such command line arguments and standard input has not been
-redirected away from the terminal then the interpreter will enter the
-interactive shell.
-
 Summary of the command line flags recognised by the interpreter:
-    -f --file           interpret contents of file as APL expressions
+    -l --log  <file>    log session input and output to <file>
+    -f --file <file>    interpret contents of <file> as APL expressions
+    -i --input <file>   redirect so that ⎕ and ⍞ are read from <file>
+    -o --output <file>  record output from ⎕← and ⍞← in <file>
 
     -h --help           print this help text and exit
     -V --Version        print version information and exit
@@ -96,17 +48,136 @@ Summary of the command line flags recognised by the interpreter:
     -s --silent         suppress interactive output
     -v --verbose        resume interactive output
 
-    --                  ends of flags
+    --                  ends of flags (what follows are arguments, if any)
 
 Note that at present in the interactive interpreter:
 
-    ^D  (end of input)  is equivalent to )OFF
-    ^C  (interrupt)     will abort execution and quit the interpreter
+    ^C  (interrupt)     may be used to abort execution and quit the interpreter
+    ^D  (end of input)  may be used to exit the interpreter instead of )OFF
 
 This program is still under active development:  any and all features are
 'as is' and subject to change.
 
 Your curiosity is much appreciated. Thank you.
+
+
+
+Logging and Scripting
+---------------------
+
+Logging and scripting is supported through command line flags.
+
+It is possible to log the entire session to file:
+
+    $ apl.py -l log_file
+
+has the same effect as:
+
+    $ apl.py |& tee -i log_file
+
+except the second inconveniently disables readline editing.
+
+Non-interactive (or scripted) use is supported:
+
+    $ apl.py < script_file
+    $ cat script_file | apl-py
+    $ apl.py -f script_file
+
+will all interpret script_file as a sequence of APL expressions.
+
+APL allows for user functions to interact with the user explicitly via
+the ⎕ and ⍞.  Such explicit input may be redirected using the -i flag:
+
+    $ apl.py -i prompt_responses -f script_file
+
+If the explicit input is not redirected in this way, user responses are
+taken from standard input regardless of the use of the -f script_file.
+
+In a similar fashion, explicit output may be redirected using the -o flag.
+This redirection is independent of the use of the -l log_file flag.
+
+
+Silent and Verbose Modes
+------------------------
+
+When using the interpreter interactively, by default, the interpreter issues
+prompts and prints the results of evaluating individual APL expressions.
+
+This output is generally not required in non-interactive mode and can be
+turned off with the -s (or --silent) flag and on with the -v (or --verbose).
+
+Silent mode does not turn off explicit output produced by ⎕ and ⍞ functions.
+In silent mode, the contents of any -l log_file are almost identical to that of
+a -o out_file but, should the interpreter crash, the former should contain a
+Python call traceback, while the latter will not.
+
+In silent mode, should apl-py raise an error (such as DOMAIN ERROR), then
+silent mode will be broken in order to print an error message and diagnostics.
+
+In script mode, apl-py will exit after reporting such an error and will not
+attempt to interpret any further APL expressions.
+
+
+Extended Script Modes
+---------------------
+
+The interpreter will exit whenever if encounters an )OFF command (or similar)
+in a script.  This is the normal way of terminating an APL script.
+
+If there is no )OFF command, the script file may be thought of an as rc file
+needed to set up an appropriate working environment, perhaps by loading an
+APL workspace.
+
+If there are more command line flags, these are be processed so it is possible
+to interpret several scripts one after the other.
+
+    $ apl.py -f file_1 -f file_2 ...
+
+If there are no more command line flags but there are arguments, the arguments
+will be interpreted as a APL expression, the result printed and the interpreter
+then will exit.
+
+    $ apl.py -i white-mice -f hhgg -- askTheQuestion
+    42
+
+Otherwise, the interpreter will enter the interactive APL shell.
+
+
+Executable Scripts
+------------------
+
+The "first line specifies the interpreter for the program" convention is
+respected.  Make the script executable and ensure the first line reads:
+
+    # !/usr/bin/apl.py --script -f
+
+Use the path appropriate for your installation.  The -f flag must appear to the
+right of any other flags.
+
+This last detail gives rise to a small limitation.  As soon as the -f flag is
+found, the contents of the script will be interpreted.  Thus any input file,
+output file and log file must have already be specified.  Thus:
+
+    $ apl.py -l log -o output -i input -f script
+
+will work and do what is wanted but:
+
+    $ apl.py -f script -l log -o output -i input
+
+may also work but the log, output and input files will not be opened until the
+last line in the script file has been interpreted, which is almost certainly
+not what is wanted.
+
+Similarly:
+
+    $ executable_script -l log -o output -i input
+
+will not give what is wanted but changing the first line of the executable
+script to:
+
+    # !/usr/bin/apl.py -l log -o output -i input -f
+
+may not be satisfactory either.
 """
 
 # ------------------------------
