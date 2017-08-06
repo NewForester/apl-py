18e7e5c43ead4111fa49916d5b5d4dda148194df
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-08-06 12:24:36 +0000

    refactor the workspace variables module

--- workspaceVariables.py
+++ workspaceVariables.py
@@ -1,5 +1,5 @@
 """
-    APL System Variables
+    APL Workspace Variables
 
     UNDER DEVELOPMENT
 
@@ -14,36 +14,52 @@ class   _WorkspaceVariable(object):
     """
     a simple value, not even a checker
     """
-    def __init__(self,value):
-        self.value = value
+    def __init__(self, quantity):
+        self._quantity = quantity
+
+    def get(self):
+        """
+        return the APL quantity
+        """
+        return self._quantity
+
+    def set(self, quantity):
+        """
+        set the APL quantity
+        """
+        self._quantity = quantity
 
 # ------------------------------
+
+# a simple dictionary with (k, v) = (name, workspace-variable)
 
 _WorkspaceVariables = {}
 
 # ------------------------------
 
-def     workspaceVariable(name,value=None):
+def     workspaceVariable(name, value=None):
     """
-    return the value of a workspace variable
+    set or get the value of a workspace variable by name lookup
 
     if a value is passed in, it is assigned to the variable and the value is returned
 
-    a new variable is created if necessary
+    a new variable is created if appropriate
     """
-    try:
-        apl_var = _WorkspaceVariables[name]
-
-        if not value is None:
-            apl_var.value = value
-    except KeyError:
-        if value is None:
+    if value is None:
+        try:
+            apl_var = _WorkspaceVariables[name]
+        except KeyError:
             apl_error("UNKNOWN VARIABLE")
+    else:
+        try:
+            apl_var = _WorkspaceVariables[name]
 
-        apl_var = _WorkspaceVariable(value)
+            apl_var.set(value)
+        except KeyError:
+            apl_var = _WorkspaceVariable(value)
 
-        _WorkspaceVariables[name] = apl_var
+            _WorkspaceVariables[name] = apl_var
 
-    return apl_var.value
+    return apl_var.get()
 
 # EOF
