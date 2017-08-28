1985dbaae58ea6e2ab03778322d69db6d141d92f
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2017-08-28 17:09:24 +0000

    add new (empty) test modules that use the new base module for tests

---
+++ testMixed.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    doctest style unit tests for APL mixed vectors
+
+    WIP - grows as more functions are implemented.
+
+    The tests in this module exercise monadic and dyadic functions with
+
+        either one or two mixed vector operands
+
+    Other cases are covered in other test modules.
+
+    Each test passes an APL expression to the evaluate function.
+    Both positive and negative (e.g. DOMAIN ERROR) cases are tested.
+
+    Note:
+        testDyadic --eager      # run with eager evaluation
+        testDyadic --lazy       # run with lazy evaluation
+"""
+
+from test.base import preamble, testResult as test
+
+# ------------------------------
+
+if __name__ == "__main__":
+    preamble()
+    import doctest
+    doctest.testmod()
+
+# EOF
